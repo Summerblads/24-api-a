16382a654721d8765ea9fe690e95cddc4fcea588
NicolasMp3 <164546658+NicolasMp3@users.noreply.github.com>
2025-03-13 18:33:01 +0000

    a

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
   console.log("Rota / solicitada");
   // Cria a rota da raiz do projeto
   res.json({
-    nome: "Arthur Porto", // Substitua pelo seu nome
+    nome: "Nicolas", // Substitua pelo seu nome
   });
 });
 
